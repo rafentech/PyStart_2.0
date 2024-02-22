59222fd9f6108ee317373afae85d63cd225efa27
Rafał Kościelny <rafen.tech@int.pl>
2024-02-22 22:32:33 +0100

    0012_HomeWork First task added

---
+++ MODULE1/0012_HomeWork_Celsius2Farenheits.py
@@ -0,0 +1,8 @@
+# Lekcja 7 Zadanie Domowe 1
+# Przygotuj program, który zamieni stopnie w Celsjuszach na Fahrenheity
+# T(°F) = T(°C) × 9/5 + 32
+
+temperatureC = float(input(f'Podaj temperaturę w stopniach Celsjusza: '))
+print("Trwa przeliczanie...")
+temperatureF = float(temperatureC * 9/5 + 32)
+print(f"Podana przez Ciebie temperatura {temperatureC}C po przeliczeniu na Farenheity to: {temperatureF}F ")
